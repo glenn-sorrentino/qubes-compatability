b997d628a77a34bfe3b402b31d050061224b7fc0
Glenn <hello@glennsorrentino.com>
2023-04-03 11:44:28 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Update package lists and install required packages
-sudo apt-get update
-sudo apt-get install -y python3 python3-pip python3-venv nginx
+sudo apt update && sudo apt -y dist-upgrade
+sudo apt install -y python3 python3-pip python3-venv nginx
 
 # Create a virtual environment
 python3 -m venv venv
