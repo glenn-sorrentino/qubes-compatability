13d39a717f08041638a7c50bc6f7636aca9fe15e
Glenn <hello@glennsorrentino.com>
2023-04-03 14:16:55 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,69 @@ source venv/bin/activate
 # Install Flask, Beautiful Soup, and Requests
 pip install Flask beautifulsoup4 requests gunicorn
 
+# Create app.py file
+cat > app.py << EOL
+from flask import Flask, render_template_string
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+url = 'https://www.qubes-os.org/hcl/'
+html_content = requests.get(url).content
+soup = BeautifulSoup(html_content, 'html.parser')
+
+table = soup.find_all('table', class_='table table-hover table-bordered table-responsive')[0]
+
+rows = table.find_all('tr')
+
+filtered_rows = []
+
+for row in rows:
+    row_data = row.find_all('td', class_='success text-center')
+    if len(row_data) == 5:
+        filtered_rows.append(row)
+
+header = rows[0]
+header.name = 'thead'
+
+new_table = soup.new_tag('table', **{'class': 'table table-hover table-bordered table-responsive'})
+new_table.append(header)
+
+for filtered_row in filtered_rows:
+    new_table.append(filtered_row)
+
+@app.route('/')
+def index():
+    return render_template_string('''<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Qubes HCL</title>
+</head>
+<body>
+<div class="container">
+    <h1>Qubes Hardware Compatibility List</h1>
+    <p>Showing only laptops with "yes" marked in all columns.</p>
+    {{ new_table | safe }}
+</div>
+</body>
+</html>''', new_table=str(new_table))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+EOL
+
+# Create wsgi.py file
+cat > wsgi.py << EOL
+from app import app
+
+if __name__ == "__main__":
+    app.run()
+EOL
+
 # Install Nginx
 sudo apt-get install -y nginx
 
@@ -44,4 +107,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
 
 # Run the app with Gunicorn
-gunicorn --bind 127.0.0.1:8000 app:app
+gunicorn --bind 127.0.0.1:8000 wsgi:app
